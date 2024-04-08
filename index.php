554d31ad2b7fd25adffaab11ca6f68e9d91389af
Slymee <jetli_rijan@hotmail.com>
2024-04-08 10:02:32 +0545

    Module for Post: wip && read.md file

--- index.php
+++ index.php
@@ -2,5 +2,19 @@
     session_start();
     require('./Partials/check-login.php');
     if(!isLoggedIn())
+    {
+        $_SESSION['message'] = "Please Login to continue.";
         header('location: ./login-register.php');
+    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
