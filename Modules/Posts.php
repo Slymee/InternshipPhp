a34d3567f080765e23b15342b796ead5b7ead3b1
Slymee <jetli_rijan@hotmail.com>
2024-04-11 09:56:55 +0545

    Post: db operation changes

--- Modules/Posts.php
+++ Modules/Posts.php
@@ -9,6 +9,8 @@ require_once('../Partials/Database.php');
 
 class Posts{
 
+
+
     public static function create($entryTitle, $date, $entryContent, $username)
     {
         $db = new Database();
@@ -27,21 +29,15 @@ class Posts{
         $filePath = 'D:\wamp64\www\InternshipPhp\Storage/'. $fileName;
         file_put_contents($filePath, $entryContent);
 
-        $userIdSQL = "SELECT id FROM users WHERE username = :username";
-        $selectStatement = $conn->prepare($userIdSQL);
-        $selectStatement->bindParam(':username', $username);
-        $selectStatement->execute();
 
-        $userId = $selectStatement->fetchColumn();
-
-        $columnNames = ['user_id', 'entry_title', 'file_path', 'created_at'];
-        $columnValues = [':user_id', ':entry_title', ':file_path', ':created_at'];
+        $columnNames = ['username', 'entry_title', 'file_path', 'created_at'];
+        $columnValues = [':username', ':entry_title', ':file_path', ':created_at'];
 
         $columns = implode(", ", $columnNames);
         $values = implode(", ", $columnValues);
 
         $insertStatement = $conn->prepare("INSERT INTO posts ($columns) VALUES ($values)");
-        $insertStatement->bindParam(':user_id', $userId);
+        $insertStatement->bindParam(':username', $username);
         $insertStatement->bindParam(':entry_title', $entryTitle);
         $insertStatement->bindParam(':file_path', $filePath);
         $insertStatement->bindParam(':created_at', $date);
@@ -49,12 +45,5 @@ class Posts{
         $insertStatement->execute();
 
         $db->closeConnection();
-    }
-
-
-    
-    public static function update()
-    {
-
     }
 }
